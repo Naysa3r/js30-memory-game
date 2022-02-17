c6adb3f0677a0da9ff627c8d08f1eb852fbc5446
Naysay3r <ilia.mahnach@gmail.com>
2022-02-17 22:54:00 +0300

    feat: fix bugs with open cards

--- scripts.js
+++ scripts.js
@@ -1,11 +1,15 @@
 const cards = document.querySelectorAll('.memory-card');
 let hasFlippedCard = false;
 let firstCard, secondCard;
+let lockBoard = false;
 
 
 function flipCard() {
-    this.classList.add('flip');
+    if (lockBoard) return;
+    if (this === firstCard) return;
 
+    this.classList.add('flip');
+    
     if (!hasFlippedCard) {
             hasFlippedCard = true;
             firstCard = this;
@@ -13,7 +17,7 @@ function flipCard() {
     }
 
     secondCard = this;
-    hasFlippedCard = false;
+    // hasFlippedCard = false;
     checkForMatch();
 }
 
@@ -28,13 +32,25 @@ function checkForMatch() {
 function disableCards() {
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
 }
 
 function unflipCards() {
+    lockBoard = true;
+
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
+
+        // lockBoard = false;
+        resetBoard();
     }, 1500);
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
