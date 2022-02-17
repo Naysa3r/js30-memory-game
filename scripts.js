8223bd7bc1b97ee48986508c991dda791c952b88
Naysay3r <ilia.mahnach@gmail.com>
2022-02-17 22:22:44 +0300

    add: add check flip cards

--- scripts.js
+++ scripts.js
@@ -1,7 +1,40 @@
 const cards = document.querySelectorAll('.memory-card');
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 
 function flipCard() {
-  this.classList.toggle('flip');
+    this.classList.add('flip');
+
+    if (!hasFlippedCard) {
+            hasFlippedCard = true;
+            firstCard = this;
+            return;
+    }
+
+    secondCard = this;
+    hasFlippedCard = false;
+    checkForMatch();
+}
+
+function checkForMatch() {
+    if (firstCard.dataset.hero === secondCard.dataset.hero) {
+        disableCards();
+        return;
+    }
+    unflipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+}
+
+function unflipCards() {
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+    }, 1500);
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
